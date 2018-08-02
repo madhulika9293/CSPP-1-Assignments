2c1b2f57ccd062df1d6c52aec4aa51ba91c0ff56
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-02 17:31:58 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- m4/p3/longest_substring.py
+++ m4/p3/longest_substring.py
@@ -2,22 +2,22 @@
 in which the letters occur in alphabetical order"""
 
 def main():
+    """ This is the code for M4-A1"""
     str_inp = input()
-    str_inp = str(input("Enter input string: "))
     count_out = 0
     check_inp = 0
     pos_out = 0
     index_out = 0
-    for I in range(len(str_inp)-1):
-        if ord(str_inp[I]) <= ord(str_inp[I+1]):
+    for loop_var in range(len(str_inp)-1):
+        if ord(str_inp[loop_var]) <= ord(str_inp[loop_var+1]):
             check_inp += 1
         else:
             check_inp = 0
         if check_inp > count_out:
             count_out = check_inp
-            index_out = I
+            index_out = loop_var
     pos_out = index_out+1-count_out
-    print("Longest substring in alphabetical order is: " + str_inp[pos_out:pos_out+count_out+1])
+    print(str_inp[pos_out:pos_out+count_out+1])
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
