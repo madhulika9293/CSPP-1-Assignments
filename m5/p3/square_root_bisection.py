d3f98df112886453df5dc8146ba0705c164cfeab
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-03 19:12:05 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- m5/p3/square_root_bisection.py
+++ m5/p3/square_root_bisection.py
@@ -5,7 +5,7 @@ def main():
     """This is the code for M5-A3"""
     str_inp = input()
     epsilon = 0.01
-    low_inp = 1.0
+    low_inp = 0
     high_inp = str_inp
     sq_root = (high_inp + low_inp)/2.0
     while abs(sq_root**2 - str_inp) >= epsilon:
