55c9cbe9b99afa74a8f7bdc224b3a399fdedabac
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-06 17:29:59 +0530

    " -> 0 of 10 passed. pylint: 3.18/10 "

--- m7/Functions - Assignment-1/assignment1.py
+++ m7/Functions - Assignment-1/assignment1.py
@@ -1,31 +1,28 @@
 """
 A program to calculate the credit card balance after one year 
-if a person only pays the minimum monthly payment required 
+if a person only pays the minimum monthly payment required
 by the credit card company each month.
 """
 def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
     original_bal = balance
-    
     def pay_made(x):
         return x*monthlyPaymentRate
 
     def bal_mon(x):
-        return (x + x*(annualInterestRate/12))
+        return x + x*(annualInterestRate/12)
 
     i = 1
     bal_due = original_bal
-    while i<=12:
-       payment = pay_made(bal_due) 
-       unpaid_bal = bal_due - payment
-       bal_due = bal_mon(unpaid_bal)
-       # print(round(bal_due, 2))
-       i += 1
+    while i <= 12:
+        payment = pay_made(bal_due) 
+        unpaid_bal = bal_due - payment
+        bal_due = bal_mon(unpaid_bal)
+        # print(round(bal_due, 2))
+        i += 1
 
     return(round(bal_due, 2))
-    print(float(bal_due))
+    print("Remaining balance: " + str(bal_due))
     
-    
-
 def main():
     data = input()
     data = data.split(' ')
@@ -34,4 +31,3 @@ def main():
 
 if __name__== "__main__":
     main()
-
