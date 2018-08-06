2acdd868cf2ee18c3994600e42bf0181aa1142cc
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-06 18:26:10 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- m7/Functions - Assignment-2/assignment2.py
+++ m7/Functions - Assignment-2/assignment2.py
@@ -4,19 +4,20 @@ in order pay off a credit card balance within 12 months
 """
 
 def payingdebtoff_inayear(balance, annual_interestrate):
-
+    """
+    A function to calculate the lowest payment
+    """
     original_bal = balance
-    
     def bal_mon(num_inp):
         return num_inp + num_inp*(annual_interestrate/12)
-    
     bal_due = original_bal
     pay_made = 10
     while bal_due > 0:
-        for loop_var in range(1,13):
+        for loop_var in range(1, 13):
             payment = pay_made
             unpaid_bal = bal_due - payment
             bal_due = bal_mon(unpaid_bal)
+            loop var == loop_var
         pay_made += 10
     return pay_made
 
@@ -27,7 +28,7 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: "+ str(payingdebtoff_inayear(data[0],data[1])))
+    print("Lowest Payment: "+ str(payingdebtoff_inayear(data[0], data[1])))
 
 if __name__ == "__main__":
     main()
