49d7290120dfa803f976103c529bf56cf2d32f81
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-07 17:04:02 +0530

    " -> 10 of 10 passed. pylint: 8.89/10 "

--- m8/p1/assignment1.py
+++ m8/p1/assignment1.py
@@ -2,20 +2,23 @@
 Python program to calculate factorial of a given integer using recursion
 '''
 
-def factorial(n):
+def factorial(num_inp):
     '''
-    n is positive Integer
+    num_inp is positive Integer
 
-    returns: a positive integer, the factorial of n.
+    returns: a positive integer, the factorial of num_inp.
     '''
-    if n <= 1:
+    if num_inp <= 1:
         return 1
-    return n*factorial(n-1)
+    return num_inp*factorial(num_inp-1)
 
 
 def main():
-    a = int(input())
-    print(factorial(int(a)))    
+    '''
+    Main function to upon the declared function
+    '''
+    num_out = int(input())
+    print(factorial(int(num_out)))    
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
