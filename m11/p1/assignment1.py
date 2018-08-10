9d1d300d5a2d7db16fe54eda65353a45a7d88b68
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-10 18:27:17 +0530

    " -> 0 of 9 passed. pylint: 0/0 "

--- m11/p1/assignment1.py
+++ m11/p1/assignment1.py
@@ -6,7 +6,7 @@ get_word_score should accept as input a string of lowercase letters
 (a word) and return the integer score for that word, using the game's scoring rules.
 '''
 
-def get_word_score(word, n):
+def get_word_score(word, hand_size):
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -21,17 +21,17 @@ def get_word_score(word, n):
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
-    SCRABBLE_LETTER_VALUES = {
+    scrabble_letter_values = {
         'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
         'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
         's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
         }
-    sum = 0
+    score_word = 0
     for char in word:
-        sum += SCRABBLE_LETTER_VALUES[char]
-    if len(word) == n:
-        return (sum*len(word))+50
-    return sum*len(word) 
+        score_word += scrabble_letter_values[char]
+    if len(word) == hand_size:
+        return  score_word*len(word))+50
+    return  score_word*len(word)
 
 
 def main():
