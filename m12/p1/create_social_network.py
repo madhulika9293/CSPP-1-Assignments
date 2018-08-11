746a1d0fce07e539c4a244788d20c07968f4d44c
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-11 09:52:23 +0530

    " -> 0 of 3 passed. pylint: 8.82/10 "

--- m12/p1/create_social_network.py
+++ m12/p1/create_social_network.py
@@ -41,7 +41,7 @@ def create_social_network(data):
     adict = {}
     for (i, j) in enumerate(inp_1):
         inp_1[i] = j.split(" follows ")
-        adict[] = j[1]
+        # adict[] = j[1]
 
     return inp_1
 
