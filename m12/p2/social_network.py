683ee2ed872d0512dd78da04f502bb33542f2627
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-11 10:43:50 +0530

    " -> 0 of 2 passed. pylint: 3.60/10 "

--- m12/p2/social_network.py
+++ m12/p2/social_network.py
@@ -15,7 +15,8 @@ def follow(network, arg1, arg2):
     '''
     # remove the pass below and start writing your code
     if arg1 in network:
-        network[arg1] = network[arg1].extend(arg2) 
+        network[arg1] = network[arg1].extend(arg2)
+    print(network[arg1])
     return network
 
 def unfollow(network, arg1, arg2):
