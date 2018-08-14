7031194c6e9169b4be6fc5b134c6fa832eb6d5e6
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-14 19:57:55 +0530

    " -> 3 of 3 passed. pylint: 9.62/10 "

--- m14/CodeCampPoker/poker.py
+++ m14/CodeCampPoker/poker.py
@@ -46,7 +46,7 @@ def is_straight(hand):
         Write the code for it and return True if it is a straight else return False
     '''
     # return is_ascending(hand_value(hand)[0])
-    card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
+    card_values = set('--23456789TJQKA'.index(c) for c, s in hand)
     return len(card_values) == 5 and (max(card_values)-min(card_values) == 4)
 
 def is_flush(hand):
