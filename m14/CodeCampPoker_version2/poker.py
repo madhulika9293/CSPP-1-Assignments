83bfc0adba45eff8ca03c5d67bb2a5a32b7464f0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-15 15:29:17 +0530

    " -> 5 of 8 passed. pylint: 9.76/10 "

--- m14/CodeCampPoker_version2/poker.py
+++ m14/CodeCampPoker_version2/poker.py
@@ -43,6 +43,8 @@ def hand_rank(hand):
         return 1
     if is_four_of_a_kind(hand):
         return 2
+    if is_fullhouse(hand):
+        return 3
     if is_flush(hand):
         return 4
     if is_straight(hand):
