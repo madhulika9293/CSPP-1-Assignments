cca21cfa43d43e599648d45c214e15eadb3615bb
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-16 17:27:22 +0530

    " -> 19 of 19 passed. pylint: 8.67/10 "

--- m15/CodeCampPoker/poker.py
+++ m15/CodeCampPoker/poker.py
@@ -54,31 +54,29 @@ def is_onepair(hand):
 def hand_rank(hand):
     ''' ranks the card '''
     hand_ranks = crd_v(hand)
-    rank = 0
+    rank = (0, hand_ranks)
     if is_flush(hand) and is_straight(hand):
-        rank = 8
+        rank = (8,)
     # four of a kind
     elif kind(hand, 4):
-        rank = 7
+        rank = (7,)
     # full house
     elif kind(hand, 3) and kind(hand, 2):
-        rank = 6
+        rank = (6, )
     elif is_flush(hand):
-        rank = 5
+        rank = (5, hand_ranks)
     elif is_straight(hand):
-        rank = 4
+        rank = (4, hand_ranks)
     # three of a kind
     elif kind(hand, 3):
-        return (rank, kind(hand, 2))
+        return (3, kind(hand, 2), hand_ranks)
     # Two pair
     elif kind(hand, 2) and kind(sorted(hand, reverse = True), 2) and kind(hand, 2) != kind(sorted(hand, reverse = True), 2):
-        return (rank, kind(hand, 2))
+        return (2, kind(hand, 2), kind(sorted(hand, reverse = True), 2))
     # one pair
     elif kind(hand, 2):
-        return (rank, kind(hand, 2))
-    else:
-        rank = 0
-    return (rank, hand_ranks)
+        return (1, kind(hand, 2))
+    return rank
 
 def poker(hands):
     '''
