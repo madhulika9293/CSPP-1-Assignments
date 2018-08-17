9b9db8abbc19dd692528a035d36b4c6c45c2cbc9
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-17 12:51:09 +0530

    " -> 0 of 3 passed. pylint: 8.78/10 "

--- m16/CodeCampDocumentDistance/document_distance.py
+++ m16/CodeCampDocumentDistance/document_distance.py
@@ -35,15 +35,15 @@ def similarity(input1, input2):
     '''
         Compute the document distance as given in the PDF
     '''
-    dict_inp1 = rem_stop_words(word_dict(input1)) 
+    dict_inp1 = rem_stop_words(word_dict(input1))
     dict_inp2 = rem_stop_words(word_dict(input2))
     # print(dict_inp1)
     # print(dict_inp2)
-    
+
     com_dict = {}
     com_words = set(list(dict_inp1.keys()) + list(dict_inp2.keys()))
     # print(com_words)
-    
+
     for loop_var in com_words:
         if loop_var in dict_inp1 and loop_var in dict_inp2:
             com_dict[loop_var] = [dict_inp1[loop_var], dict_inp2[loop_var]]
@@ -54,13 +54,13 @@ def similarity(input1, input2):
 
     sim_num = sum([com_dict[word][0]*com_dict[word][1] for word in com_dict])
     sim_den1 = math.sqrt(sum([com_dict[word][0]**2 for word in com_dict]))
-    sim_den2 = math.sqrt(sum([com_dict[word][1]**2 for word in com_dict])) 
+    sim_den2 = math.sqrt(sum([com_dict[word][1]**2 for word in com_dict]))
 
     cos_dist = sim_num/(sim_den1*sim_den2)
 
     return cos_dist
 
-def load_stopwords(filename):
+def load_stopwords(filename1):
     '''
         loads stop words from a file and returns a dictionary
     '''
