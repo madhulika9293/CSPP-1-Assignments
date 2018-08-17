5f2f96b26cb679ec663ad4d5778506cc7a467717
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-17 14:41:12 +0530

    " -> 0 of 3 passed. pylint: 5.12/10 "

--- m16/CodeCampDocumentDistance/document_distance.py
+++ m16/CodeCampDocumentDistance/document_distance.py
@@ -13,23 +13,14 @@ def word_dict(inp):
         if char in '!~@#$%^&*.?':
             inp = inp.replace(char, '')
     inp = inp.strip().split(" ")
-    i_1 = {(val if val not in stopwords):(inp.count(val)) for val in inp}
-    # for val in inp:
-    #     if val not in stop_words:
-    #         if val in i_1:
-    #             i_1[val] += 1
-    #         else:
-    #             i_1[val] = 1
+    for val in inp:
+        if val not in stop_words:
+            if val in i_1:
+                i_1[val] += 1
+            else:
+                i_1[val] = 1
     return i_1
 
-# def rem_stop_words(dict1):
-#     '''
-#         Removes stop words from the input
-#     '''
-#     for val in list(dict1):
-#         if val in stop_words:
-#             del dict1[val]
-#     return dict1
 
 def similarity(input1, input2):
     '''
@@ -37,13 +28,9 @@ def similarity(input1, input2):
     '''
     dict_inp1 = word_dict(input1)
     dict_inp2 = word_dict(input2)
-    # print(dict_inp1)
-    # print(dict_inp2)
-    # print(word_dict(input1))
 
     com_dict = {}
     com_words = set(list(dict_inp1.keys()) + list(dict_inp2.keys()))
-    # print(com_words)
 
     for loop_var in com_words:
         if loop_var in dict_inp1 and loop_var in dict_inp2:
@@ -53,7 +40,6 @@ def similarity(input1, input2):
         elif loop_var not in dict_inp1 and loop_var in dict_inp2:
             com_dict[loop_var] = [0, dict_inp2[loop_var]]
 
-    # print(com_dict)
     sim_num = sum([com_dict[word][0]*com_dict[word][1] for word in com_dict])
     sim_den1 = math.sqrt(sum([com_dict[word][0]**2 for word in com_dict]))
     sim_den2 = math.sqrt(sum([com_dict[word][1]**2 for word in com_dict]))
