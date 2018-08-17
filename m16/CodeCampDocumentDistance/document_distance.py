b094b77c79420fd68476fded895dbed3ceed7bf0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-17 14:45:20 +0530

    " -> 0 of 3 passed. pylint: 9.78/10 "

--- m16/CodeCampDocumentDistance/document_distance.py
+++ m16/CodeCampDocumentDistance/document_distance.py
@@ -45,6 +45,7 @@ def similarity(input1, input2):
     sim_den1 = math.sqrt(sum([com_dict[word][0]**2 for word in com_dict]))
     sim_den2 = math.sqrt(sum([com_dict[word][1]**2 for word in com_dict]))
 
+    print(com_dict)
     print(sim_num, sim_den1, sim_den2)
 
     cos_dist = sim_num/(sim_den1*sim_den2)
