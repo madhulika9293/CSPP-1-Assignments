849e297da1c6817524776c33c425f5f8f9dc87a0
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-18 10:52:54 +0530

    " -> 0 of 1 passed. pylint: 8.67/10 "

--- m18/p2/search.py
+++ m18/p2/search.py
@@ -80,7 +80,8 @@ def main():
             print(search_index[word])
             for i in search_index[word]:
                 print(i, i[0])
-                # list_of_id += i[0]
+                list_of_id = list_of_id.extend(i[0])
+    print(list_of_id)
 
     # call process queries
     # process_queries(search_index, queries)
