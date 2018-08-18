763231e9ed9ad822be98f92779b0f79994120a45
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-18 10:48:39 +0530

    " -> 0 of 1 passed. pylint: 7.62/10 "

--- m18/p2/search.py
+++ m18/p2/search.py
@@ -37,7 +37,14 @@ def search(search_index, query):
         collect all the values for the words that are in the search_index
         make a set of doc_id and return
     '''
-    pass
+    query = query.lower().split(' ')
+    list_of_id = []
+    for word in query:
+        if word in search_index:
+            for i in search_index[word]:
+                list_of_id += i[0]
+    return set(list_of_id)        
+
 
 def process_queries(search_index, queries):
     '''
@@ -45,7 +52,9 @@ def process_queries(search_index, queries):
         iterate through all the queries and call the search function
         print the results returned by search function
     '''
-    pass
+    for each in queries:
+        print(search(search_index, each))
+    
 
 def main():
     '''
@@ -62,8 +71,7 @@ def main():
     for i in range(lines):
         queries.append(input())
         i += 1
-    print(search_index)
-    print(queries)
+
     # call process queries
     process_queries(search_index, queries)
 
