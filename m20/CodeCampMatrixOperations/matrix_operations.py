a0f27f4f83ea7423b285a47930cc18750e9f1a17
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-23 09:58:34 +0530

    " -> 0 of 8 passed. pylint: 3.68/10 "

---
+++ m20/CodeCampMatrixOperations/matrix_operations.py
@@ -0,0 +1,55 @@
+def mult_matrix(m1, m2):
+    '''
+        check if the matrix1 columns = matrix2 rows
+        mult the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for mult
+        and return None
+        error message should be "Error: Matrix shapes invalid for mult"
+    '''
+    pass
+
+def add_matrix(m1, m2):
+    '''
+        check if the matrix shapes are similar
+        add the matrices and return the result matrix
+        print an error message if the matrix shapes are not valid for addition
+        and return None
+        error message should be "Error: Matrix shapes invalid for addition"
+    '''
+    pass
+
+def read_matrix():
+    '''
+    rows = int(input())
+        read the matrix dimensions from input
+        create a list of lists and read the numbers into it
+        in case there are not enough numbers given in the input
+        print an error message and return None
+        error message should be "Error: Invalid input for the matrix"
+    '''
+    mat_rows = int(input())
+    mat_columns = int(input())
+    matrix = []
+    for i in range(mat_rows):
+        matrix += [input()]
+        if len(matrix[i]) != mat_columns:
+            print("Error: Invalid input for the matrix")
+            return None
+    return matrix
+
+def main():
+    # read matrix 1
+
+    # read matrix 2
+
+    # add matrix 1 and matrix 2
+
+    # multiply matrix 1 and matrix 2
+
+    mat_1 = read_matrix()
+    # mat_2 = read_matrix()
+    print(mat_1) 
+    # print(mat_2) 
+
+if __name__ == '__main__':
+    main()
