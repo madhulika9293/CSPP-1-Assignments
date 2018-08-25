14df54bd8257b4636d3d34cf972abc8c8a82c025
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-25 10:53:34 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

---
+++ m22/assignment4/print_dictionary.py
@@ -0,0 +1,19 @@
+'''
+Write a function to print a dictionary with the keys in sorted order along with the
+frequency of each word. Format of the printing should be one key per line and separate
+the key and frequency with a SPACE - SPACE.
+'''
+
+def print_dictionary(dictionary):
+	dict_list = [[] for _ in dictionary]
+    for i, each_key in enumerate(dictionary):
+    	dict_list[i][0] = each_key
+    	dict_list[i][1] = dictionary[each_key]
+    return dict_list
+
+def main():
+    dictionary = eval(input())
+    print_dictionary(dictionary)
+
+if __name__ == '__main__':
+    main()
