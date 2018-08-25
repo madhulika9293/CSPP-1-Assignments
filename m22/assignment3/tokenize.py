eefd6afe75efffc05a2a73546dad39f38128f5f4
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-25 10:45:30 +0530

    " -> 0 of 4 passed. pylint: 5.00/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -4,10 +4,21 @@ each word
 '''
 
 def tokenize(string):
-    pass
+    freq_dict = {}
+    lst_inp = string.split(" ")
+    for each_word in lst_inp:
+    	if each_word not in freq_dict:
+    		freq_dict[each_word] = lst_inp.count(each_word)
+    return freq_dict
+    
             
 def main():
-    pass
+    num_lines = int(input())
+    string_inp = ''
+    for _ in range(num_lines):
+    	string_inp += input()
+    	string_inp += " "
+    print(tokenize(string_inp))
 
 if __name__ == '__main__':
     main()
