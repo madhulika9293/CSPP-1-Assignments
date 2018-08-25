4a6ffa18091f5b1bf30b4858c0081ac5c7273ba1
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-25 11:15:14 +0530

    " -> 22 of 26 passed. pylint: 9.00/10 "

--- m22/Check Sudoku/check_sudoku.py
+++ m22/Check Sudoku/check_sudoku.py
@@ -27,8 +27,7 @@ def check_sudoku(sudoku):
         for each_entry in each_row:
             if each_entry not in '0123456789':
                 return False
-            else:
-                return row_check(sudoku)
+            return row_check(sudoku)
     # return sudok
 
 def main():
