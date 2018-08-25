ebe097db41e0e563424f4038a9f9f934f3794244
Madhulika <ramyamdhulika9293@msitprogram.net>
2018-08-25 10:53:55 +0530

    " -> 0 of 3 passed. pylint: 7.27/10 "

--- m22/assignment4/print_dictionary.py
+++ m22/assignment4/print_dictionary.py
@@ -5,10 +5,10 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-	dict_list = [[] for _ in dictionary]
+    dict_list = [[] for _ in dictionary]
     for i, each_key in enumerate(dictionary):
-    	dict_list[i][0] = each_key
-    	dict_list[i][1] = dictionary[each_key]
+        dict_list[i][0] = each_key
+        dict_list[i][1] = dictionary[each_key]
     return dict_list
 
 def main():
